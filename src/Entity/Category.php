06f57dc131ca4e4fea8e9698a04a9b00369efc3d
T.Michel <theo.michel2@outlook.fr>
2022-04-21 11:13:37 +0200

    testing serialisation and how groups work

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -7,6 +7,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 #[ApiResource()]
@@ -15,8 +16,10 @@ class Category
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['read:post'])]
     private $id;
 
+    #[Groups(['read:post'])]
     #[ORM\Column(type: 'string', length: 255)]
     private $name;
 
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -5,33 +5,56 @@ namespace App\Entity;
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\PostRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
-#[ApiResource()]
+#[ApiResource(
+    normalizationContext: ['groups' =>  ['read:collection']],
+    denormalizationContext: ['groups' => ['write:Post']],
+    itemOperations: [
+        'put',
+        'delete',
+        'get' => [
+        'normalization_context' => ['groups' => ['read:collection', 'read:item', 'read:post']]
+    ]]
+)]
 class Post
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
+    #[Groups(['read:collection'])]
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read:collection', 'write:Post'])]
     private $title;
 
     #[ORM\Column(type: 'string', length: 255)]
+    #[Groups(['read:collection', 'write:Post'])]
     private $slug;
 
     #[ORM\Column(type: 'text')]
+    #[Groups(['read:item', 'write:Post'])]
     private $content;
 
     #[ORM\Column(type: 'datetime')]
+    #[Groups(['read:item'])]
     private $createdAt;
 
     #[ORM\Column(type: 'datetime')]
+    #[Groups(['read:item'])]
     private $updatedAt;
 
     #[ORM\ManyToOne(targetEntity: Category::class, inversedBy: 'posts')]
+    #[Groups(['read:item', 'write:Post'])]
     private $category;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
 
     public function getId(): ?int
     {
