eae0bb8ba766b757264ce8e0ed76a52d42ecb274
T.Michel <theo.michel2@outlook.fr>
2022-04-21 18:55:41 +0200

    create persister for entity don't link with orm/doctrine

--- src/DataProvider/DependencyDataProvider.php
+++ src/DataProvider/DependencyDataProvider.php
@@ -7,42 +7,23 @@ use App\Entity\Dependency;
 use ApiPlatform\Core\DataProvider\RestrictedDataProviderInterface;
 use ApiPlatform\Core\DataProvider\ContextAwareCollectionDataProviderInterface;
 use ApiPlatform\Core\DataProvider\ItemDataProviderInterface;
+use App\Repository\DependencyRepository;
 
 class DependencyDataProvider implements ContextAwareCollectionDataProviderInterface,RestrictedDataProviderInterface,ItemDataProviderInterface{
 
-    public function __construct(private string $rootPath)
+    public function __construct(private DependencyRepository $repository)
     {
         
     }
 
-    private function getDependecies(): array{
-        $path = $this->rootPath . '/composer.json';
-        $json = json_decode(file_get_contents($path),true);
-        return $json['require'];
-    }
-
     public function getItem(string $resourceClass, $id, ?string $operationName = null, array $context = [])
     {
-        $depencies = $this->getDependecies();
-        foreach($depencies as $name => $version){
-            $uuid = Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString();
-            if($uuid === $id){
-                return new Dependency($uuid, $name, $version);
-            }
-        }
-        return null;
+        return $this->repository->find($id);
     }
 
     public function getCollection(string $resourceClass, ?string $operationName = null, array $context = [])
     {
-        $path = $this->rootPath . '/composer.json';
-        $json = json_decode(file_get_contents($path),true);
-        $items = [];
-        foreach($this->getDependecies() as $name => $version)
-        {
-            $items[] = new Dependency(Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString(), $name, $version);
-        }
-        return $items;
+        return $this->repository->findAll();
     }
 
 
--- src/Entity/Dependency.php
+++ src/Entity/Dependency.php
@@ -2,12 +2,21 @@
 
 namespace App\Entity;
 
+use Ramsey\Uuid\Uuid;
 use ApiPlatform\Core\Annotation\ApiProperty;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ApiResource(
-    itemOperations: ['get'],
-    collectionOperations: ['get'],
+    itemOperations: ['get',
+     'put'=> [
+         'denormalization_context' => ['groups' => ['put:Dependency']]
+     ],
+     'delete'
+    ],
+    collectionOperations: ['get','post'],
     paginationEnabled: false
 )]
 class Dependency{
@@ -17,9 +26,11 @@ class Dependency{
     )]
     private string $uuid;
 
-    #[ApiProperty(
-        description: 'Nom de la dépendance',
-    )]
+    #[
+        ApiProperty(description: 'Nom de la dépendance',),
+        Length(min:2),
+        NotBlank()
+    ]
     private string $name;
 
     #[ApiProperty(
@@ -27,12 +38,14 @@ class Dependency{
         openapiContext: [
             'example' => '5.2.*'
         ]
-    )]
+        ),
+        Groups('put:Dependency')
+    ]
     private string $version;
 
-    public function __construct(string $uuid, string $name, string $version)
+    public function __construct(string $name, string $version)
     {
-        $this->uuid = $uuid;
+        $this->uuid = Uuid::uuid5(Uuid::NAMESPACE_URL, $name)->toString();;
         $this->name = $name;
         $this->version = $version;
     }
@@ -65,5 +78,20 @@ class Dependency{
     public function getVersion(): string
     {
         return $this->version;
+    }
+
+
+    /**
+     * Set the value of version
+     *
+     * @param string $version
+     *
+     * @return self
+     */
+    public function setVersion(string $version): self
+    {
+        $this->version = $version;
+
+        return $this;
     }
 }
